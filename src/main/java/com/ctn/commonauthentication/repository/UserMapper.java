6f4c9490449f4066169334a3c918d2f73681ab55
LahiruMadushann <lahirumadushandl@gmail.com>
2026-02-27 09:30:56 +0530

    fix issue in user details mybatis codes

--- src/main/java/com/ctn/commonauthentication/repository/UserMapper.java
+++ src/main/java/com/ctn/commonauthentication/repository/UserMapper.java
@@ -8,11 +8,7 @@ public interface UserMapper {
     public User find(String username) ;
     public User findById(Long id);
 
-    public void updatePass(String user_id, String newPass);
     public void add(String user_id, String pass, String role);
     public Integer getId(String username);
-    public String getEmail(int id);
-
-    public void updateUserName(String email, String newEmail);
 
 }
